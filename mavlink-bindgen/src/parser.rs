800fb01929953f043adccb77a06583df8f2166d7
f <7368920+fakerror@users.noreply.github.com>
2025-04-27 16:27:52 +0800

    add more bitmask field

--- mavlink-bindgen/src/parser.rs
+++ mavlink-bindgen/src/parser.rs
@@ -1204,6 +1204,15 @@ pub fn parse_profile(
                                 }
                                 _ => (),
                             }
+                            // Special case for MavMode
+                            if field.enumtype == Some("MavMode".to_string()) {
+                                field.enumtype = Some("MavModeFlag".to_string());
+                                field.display = Some("bitmask".to_string());
+                            }
+                            // Special case for field having "flags" in the name
+                            if field.name.contains("flags") {
+                                field.display = Some("bitmask".to_string());
+                            }
                         }
                         Some(&MavXmlElement::Param) => {
                             if entry.params.is_none() {
